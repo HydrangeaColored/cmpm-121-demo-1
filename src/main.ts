bb19479930b24f285e1fe0a74c4f9aeeba9d40f2
Steven <stevenren999@gmail.com>
2023-10-08 07:04:33 -0700

    Stage 2 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,29 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My games";
+const gameName = "🍟 Fries Game 🍟";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+let currCount = 0;
+
+const counter = document.createElement("div");
+counter.innerHTML = `You've eaten ${currCount} fries! 😔`;
+app.append(counter);
+
+const button = document.createElement("button");
+button.innerHTML = "Eat a fry!";
+app.append(button);
+
+button.addEventListener("click", () => {
+  currCount++;
+  if (currCount == 1) {
+    counter.innerHTML = `You've eaten ${currCount} fry! 👍`;
+  } else {
+    counter.innerHTML = `You've eaten ${currCount} fries! 🍟🍟🍟`;
+  }
+});
